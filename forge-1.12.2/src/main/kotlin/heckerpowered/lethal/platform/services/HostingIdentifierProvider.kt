984fc50517c036ab3b7eec89783589f7cd9ce83a
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-05 12:53:00 +0530

    Rename IdentifierProvider to HostingIdentifierProvider for clarity

--- forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/services/HostingIdentifierProvider.kt
+++ forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/services/HostingIdentifierProvider.kt
@@ -4,7 +4,7 @@ import heckerpowered.lethal.bridge.resources.Identifier
 import heckerpowered.lethal.bridge.resources.IdentifierProvider
 import net.minecraft.util.ResourceLocation
 
-class IdentifierProvider : IdentifierProvider {
+class HostingIdentifierProvider : IdentifierProvider {
     @Suppress("KotlinConstantConditions")
     override fun identifier(namespace: String, path: String): Identifier {
         return ResourceLocation(namespace, path) as Identifier
