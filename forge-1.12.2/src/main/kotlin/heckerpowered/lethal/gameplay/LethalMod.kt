ef20d3c2793eafe1a7afd967654f00e88439f73d
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-06 18:38:40 +0530

    Implement entrypoint loading and logging in pre-initialize method

--- forge-1.12.2/src/main/kotlin/heckerpowered/lethal/gameplay/LethalMod.kt
+++ forge-1.12.2/src/main/kotlin/heckerpowered/lethal/gameplay/LethalMod.kt
@@ -5,6 +5,9 @@
 
 package heckerpowered.lethal.gameplay
 
+import heckerpowered.lethal.bridge.platform.Services
+import heckerpowered.lethal.bridge.platform.loads
+import heckerpowered.lethal.bridge.platform.services.Entrypoint
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventHandler
@@ -35,5 +38,14 @@ class LethalMod {
     @EventHandler
     fun preInitialize(event: FMLPreInitializationEvent) {
         Logger = event.modLog
+
+        val entrypoints = Services.loads<Entrypoint>()
+        val entrypointCount = entrypoints.count()
+
+        Logger.info("Found $entrypointCount entrypoint(s).")
+        for ((index, entrypoint) in entrypoints.withIndex()) {
+            Logger.info("Calling entrypoint ($index/$entrypointCount): ${entrypoint.javaClass.name}")
+            entrypoint.onEntrypoint()
+        }
     }
 }
