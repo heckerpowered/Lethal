e16e804698039c31d7f38ea1b9f1c99affb89807
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-07 17:01:22 +0530

    Add ItemStackMixin to enhance item stack functionality with access methods

---
+++ forge-1.12.2/src/main/java/heckerpowered/lethal/mixin/ItemStackMixin.java
@@ -0,0 +1,92 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 heckerpowered
+ */
+
+package heckerpowered.lethal.mixin;
+
+import heckerpowered.lethal.bridge.adapter.item.ItemAccess;
+import heckerpowered.lethal.bridge.adapter.item.stack.ItemStackAccess;
+import heckerpowered.lethal.platform.interop.ItemInterop;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.*;
+
+@Mixin(ItemStack.class)
+@Implements(@Interface(iface = ItemStackAccess.class, prefix = "itemStackAccess$"))
+abstract class ItemStackMixin {
+    @Shadow
+    public abstract Item getItem();
+
+    @Shadow
+    public abstract int getCount();
+
+    @Shadow
+    public abstract void setCount(int size);
+
+    @Shadow
+    public abstract int getItemDamage();
+
+    @Shadow
+    public abstract void setItemDamage(int meta);
+
+    @Shadow
+    public abstract int getMaxStackSize();
+
+    @Shadow
+    public abstract int getMaxDamage();
+
+    @Shadow
+    public abstract boolean isEmpty();
+
+    @Shadow
+    public abstract boolean isItemStackDamageable();
+
+    @Shadow
+    public abstract boolean isItemDamaged();
+
+    @NotNull
+    public ItemAccess itemStackAccess$getItem() {
+        return ItemInterop.item(getItem());
+    }
+
+    @Intrinsic
+    public int itemStackAccess$getCount() {
+        return getCount();
+    }
+
+    @Intrinsic
+    public void itemStackAccess$setCount(int count) {
+        setCount(count);
+    }
+
+    public int itemStackAccess$getDamagePoints() {
+        return getItemDamage();
+    }
+
+    public void itemStackAccess$setDamagePoints(int damagePoints) {
+        setItemDamage(damagePoints);
+    }
+
+    public int itemStackAccess$getMaxStackCount() {
+        return getMaxStackSize();
+    }
+
+    public int itemStackAccess$getMaxDamagePoints() {
+        return getMaxDamage();
+    }
+
+    @Intrinsic
+    public boolean itemStackAccess$isEmpty() {
+        return isEmpty();
+    }
+
+    public boolean itemStackAccess$isDamageable() {
+        return isItemStackDamageable();
+    }
+
+    public boolean itemStackAccess$isDamaged() {
+        return isItemDamaged();
+    }
+}
