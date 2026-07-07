bbab7ba1c66feea80174fb7100685703b9ec7d3c
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-07 17:02:43 +0530

    Add ItemStackAccess interface for item stack management

---
+++ bridge/src/main/kotlin/heckerpowered/lethal/bridge/adapter/item/stack/ItemStackAccess.kt
@@ -0,0 +1,25 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 heckerpowered
+ */
+
+package heckerpowered.lethal.bridge.adapter.item.stack
+
+import heckerpowered.lethal.bridge.adapter.item.ItemAccess
+
+interface ItemStackAccess {
+    val item: ItemAccess
+    var count: Int
+    var damagePoints: Int
+    val maxStackCount: Int
+    val maxDamagePoints: Int
+
+    val isEmpty: Boolean
+        get() = count <= 0
+
+    val isDamageable: Boolean
+        get() = maxDamagePoints > 0
+
+    val isDamaged: Boolean
+        get() = damagePoints > 0
+}
