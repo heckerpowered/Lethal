ba13c612d93c141c285187931ac247c7cf78cda1
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-07 17:19:57 +0530

    Refactor ItemMixin to utilize ItemAccessImpl for method calls

--- forge-1.12.2/src/main/java/heckerpowered/lethal/mixin/ItemMixin.java
+++ forge-1.12.2/src/main/java/heckerpowered/lethal/mixin/ItemMixin.java
@@ -14,6 +14,7 @@ import heckerpowered.lethal.bridge.math.BlockDirection;
 import heckerpowered.lethal.bridge.math.BlockPositionView;
 import heckerpowered.lethal.bridge.math.VectorView;
 import heckerpowered.lethal.bridge.resources.Identifier;
+import heckerpowered.lethal.mixin.impl.ItemAccessImpl;
 import heckerpowered.lethal.platform.interop.ItemInterop;
 import net.minecraft.item.Item;
 import org.jetbrains.annotations.NotNull;
@@ -27,100 +28,101 @@ import javax.annotation.Nullable;
 @Mixin(Item.class)
 @Implements(@Interface(iface = ItemAccess.class, prefix = "itemAccess$"))
 abstract class ItemMixin {
+    @SuppressWarnings("AddedMixinMembersNamePattern")
     @Unique
-    private final Item self(){
+    private Item self() {
         return (Item) (Object) this;
     }
 
     @NotNull
     public Identifier itemAccess$getIdentifier() {
-        return ItemInterop.identifier(ItemInterop.item(this));
+        return ItemInterop.identifier(self());
     }
 
     @NotNull
     public ItemForm itemAccess$getForm() {
-        return ItemInterop.form(ItemInterop.item(this));
+        return ItemAccessImpl.form(self());
     }
 
     @NotNull
     public ItemInteractionResult itemAccess$use(@NotNull ItemStackAccess stack, @NotNull WorldAccess world, @NotNull EntityAccess user, @NotNull Hand hand) {
-        return ItemInterop.use(ItemInterop.item(this), world, user, hand);
+        return ItemAccessImpl.use(self(), world, user, hand);
     }
 
     @NotNull
     public ItemInteractionResult itemAccess$useOnBlock(@NotNull ItemStackAccess stack, @NotNull WorldAccess world, @NotNull EntityAccess user, @NotNull Hand hand, @NotNull BlockPositionView position, @NotNull BlockStateAccess blockState, @NotNull BlockDirection face, @NotNull VectorView hitPosition) {
-        return ItemInterop.useOnBlock(ItemInterop.item(this), world, user, hand, position, face, hitPosition);
+        return ItemAccessImpl.useOnBlock(self(), world, user, hand, position, face, hitPosition);
     }
 
     @NotNull
     public ItemInteractionResult itemAccess$interactLivingEntity(@NotNull ItemStackAccess stack, @NotNull WorldAccess world, @NotNull EntityAccess user, @NotNull EntityAccess target, @NotNull Hand hand) {
-        return ItemInterop.interactLivingEntity(ItemInterop.item(this), stack, user, target, hand);
+        return ItemAccessImpl.interactLivingEntity(self(), stack, user, target, hand);
     }
 
     public void itemAccess$onUseTick(@NotNull ItemStackAccess stack, @NotNull WorldAccess world, @NotNull EntityAccess user, int remainingUseTicks) {
-        ItemInterop.onUseTick(ItemInterop.item(this), stack, user, remainingUseTicks);
+        ItemAccessImpl.onUseTick(self(), stack, user, remainingUseTicks);
     }
 
     @NotNull
     public ItemStackAccess itemAccess$finishUsing(@NotNull ItemStackAccess stack, @NotNull WorldAccess world, @NotNull EntityAccess user) {
-        return ItemInterop.finishUsing(ItemInterop.item(this), stack, world, user);
+        return ItemAccessImpl.finishUsing(self(), stack, world, user);
     }
 
     public boolean itemAccess$releaseUsing(@NotNull ItemStackAccess stack, @NotNull WorldAccess world, @NotNull EntityAccess user, int remainingUseTicks) {
-        return ItemInterop.releaseUsing(ItemInterop.item(this), stack, world, user, remainingUseTicks);
+        return ItemAccessImpl.releaseUsing(self(), stack, world, user, remainingUseTicks);
     }
 
     public void itemAccess$inventoryTick(@NotNull ItemStackAccess stack, @NotNull WorldAccess world, @NotNull EntityAccess owner, @Nullable EquipmentSlot slot, @Nullable Integer slotIndex, boolean isSelected) {
-        ItemInterop.inventoryTick(ItemInterop.item(this), stack, world, owner, slotIndex, isSelected);
+        ItemAccessImpl.inventoryTick(self(), stack, world, owner, slotIndex, isSelected);
     }
 
     public void itemAccess$onCrafted(@NotNull ItemStackAccess stack, @NotNull WorldAccess world, @Nullable EntityAccess player) {
-        ItemInterop.onCrafted(ItemInterop.item(this), stack, world, player);
+        ItemAccessImpl.onCrafted(self(), stack, world, player);
     }
 
     @NotNull
     public ItemUseAnimation itemAccess$getUseAnimation(@NotNull ItemStackAccess stack) {
-        return ItemInterop.getUseAnimation(ItemInterop.item(this), stack);
+        return ItemAccessImpl.getUseAnimation(self(), stack);
     }
 
     public int itemAccess$getUseDurationTicks(@NotNull ItemStackAccess stack, @Nullable EntityAccess user) {
-        return ItemInterop.getUseDurationTicks(ItemInterop.item(this), stack);
+        return ItemAccessImpl.getUseDurationTicks(self(), stack);
     }
 
     public double itemAccess$getDestroySpeed(@NotNull ItemStackAccess stack, @NotNull BlockStateAccess blockState) {
-        return ItemInterop.getDestroySpeed(ItemInterop.item(this), stack, blockState);
+        return ItemAccessImpl.getDestroySpeed(self(), stack, blockState);
     }
 
     public boolean itemAccess$canHarvest(@NotNull ItemStackAccess stack, @NotNull BlockStateAccess blockState) {
-        return ItemInterop.canHarvest(ItemInterop.item(this), stack, blockState);
+        return ItemAccessImpl.canHarvest(self(), stack, blockState);
     }
 
     public int itemAccess$getMiningLevel(@NotNull ItemStackAccess stack, @Nullable BlockStateAccess blockState, @Nullable EntityAccess user) {
-        return ItemInterop.getMiningLevel(ItemInterop.item(this), stack, blockState, user);
+        return ItemAccessImpl.getMiningLevel(self(), stack, blockState, user);
     }
 
     public boolean itemAccess$mineBlock(@NotNull ItemStackAccess stack, @NotNull WorldAccess world, @NotNull BlockStateAccess blockState, @NotNull BlockPositionView position, @NotNull EntityAccess miner) {
-        return ItemInterop.mineBlock(ItemInterop.item(this), stack, world, blockState, position, miner);
+        return ItemAccessImpl.mineBlock(self(), stack, world, blockState, position, miner);
     }
 
     public boolean itemAccess$hurtEnemy(@NotNull ItemStackAccess stack, @NotNull WorldAccess world, @NotNull EntityAccess target, @NotNull EntityAccess attacker) {
-        return ItemInterop.hurtEnemy(ItemInterop.item(this), stack, target, attacker);
+        return ItemAccessImpl.hurtEnemy(self(), stack, target, attacker);
     }
 
     public void itemAccess$postHurtEnemy(@NotNull ItemStackAccess stack, @NotNull WorldAccess world, @NotNull EntityAccess target, @NotNull EntityAccess attacker) {
-        ItemInterop.postHurtEnemy(ItemInterop.item(this), stack, world, target, attacker);
+        ItemAccessImpl.postHurtEnemy(self(), stack, world, target, attacker);
     }
 
     @Nullable
     public EquipmentSlot itemAccess$getEquipmentSlot(@NotNull ItemStackAccess stack, @Nullable EquipmentSlot slot, @Nullable EntityAccess wearer) {
-        return ItemInterop.getEquipmentSlot(ItemInterop.item(this), stack, slot, wearer);
+        return ItemAccessImpl.getEquipmentSlot(self(), stack, slot, wearer);
     }
 
     public int itemAccess$getArmorProtectionPoints(@NotNull ItemStackAccess stack, @Nullable EquipmentSlot slot, @Nullable EntityAccess wearer) {
-        return ItemInterop.getArmorProtectionPoints(ItemInterop.item(this), stack, slot);
+        return ItemAccessImpl.getArmorProtectionPoints(self(), stack, slot);
     }
 
     public double itemAccess$getArmorToughnessPoints(@NotNull ItemStackAccess stack, @Nullable EquipmentSlot slot, @Nullable EntityAccess wearer) {
-        return ItemInterop.getArmorToughnessPoints(ItemInterop.item(this), stack, slot);
+        return ItemAccessImpl.getArmorToughnessPoints(self(), stack, slot);
     }
 }
