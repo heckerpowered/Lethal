61e73dc2247e238d79320d0f1b655b75f419e489
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-07 17:15:50 +0530

    Refactor DamageProvider to use direct DamageSource methods for improved clarity and performance

--- forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/services/DamageProvider.kt
+++ forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/services/DamageProvider.kt
@@ -9,7 +9,8 @@ import heckerpowered.lethal.bridge.adapter.entity.EntityAccess
 import heckerpowered.lethal.bridge.adapter.entity.damagesource.*
 import heckerpowered.lethal.bridge.adapter.entity.damagesource.DamageProvider
 import heckerpowered.lethal.bridge.math.VectorView
-import heckerpowered.lethal.platform.interop.ObjectInterop
+import heckerpowered.lethal.platform.interop.damageSource
+import heckerpowered.lethal.platform.interop.entityOrNull
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.entity.projectile.EntityArrow
@@ -21,85 +22,37 @@ import net.minecraft.util.EntityDamageSourceIndirect
 class DamageProvider : DamageProvider {
     private fun vanilla(spec: VanillaDamageSourceSpec, directEntity: EntityAccess?, causingEntity: EntityAccess?, position: VectorView?): DamageSourceView {
         return when (spec.type) {
-            VanillaDamageType.InFire -> ObjectInterop.damageSource(DamageSource.IN_FIRE)
-            VanillaDamageType.LightningBolt -> ObjectInterop.damageSource(DamageSource.LIGHTNING_BOLT)
-            VanillaDamageType.OnFire -> ObjectInterop.damageSource(DamageSource.ON_FIRE)
-            VanillaDamageType.Lava -> ObjectInterop.damageSource(DamageSource.LAVA)
-            VanillaDamageType.HotFloor -> ObjectInterop.damageSource(DamageSource.HOT_FLOOR)
-            VanillaDamageType.InWall -> ObjectInterop.damageSource(DamageSource.IN_WALL)
-            VanillaDamageType.Cramming -> ObjectInterop.damageSource(DamageSource.CRAMMING)
-            VanillaDamageType.Drown -> ObjectInterop.damageSource(DamageSource.DROWN)
-            VanillaDamageType.Starve -> ObjectInterop.damageSource(DamageSource.STARVE)
-            VanillaDamageType.Cactus -> ObjectInterop.damageSource(DamageSource.CACTUS)
-            VanillaDamageType.Fall -> ObjectInterop.damageSource(DamageSource.FALL)
-            VanillaDamageType.FlyIntoWall -> ObjectInterop.damageSource(DamageSource.FLY_INTO_WALL)
-            VanillaDamageType.FellOutOfWorld -> ObjectInterop.damageSource(DamageSource.OUT_OF_WORLD)
-            VanillaDamageType.Generic -> ObjectInterop.damageSource(DamageSource.GENERIC)
-            VanillaDamageType.Magic -> ObjectInterop.damageSource(DamageSource.MAGIC)
-            VanillaDamageType.Wither -> ObjectInterop.damageSource(DamageSource.WITHER)
-            VanillaDamageType.FallingAnvil -> ObjectInterop.damageSource(DamageSource.ANVIL)
-            VanillaDamageType.FallingBlock -> ObjectInterop.damageSource(DamageSource.FALLING_BLOCK)
-            VanillaDamageType.DragonBreath -> ObjectInterop.damageSource(DamageSource.DRAGON_BREATH)
-            VanillaDamageType.Fireworks -> ObjectInterop.damageSource(DamageSource.FIREWORKS)
-
-            VanillaDamageType.MobAttack -> ObjectInterop.damageSource(
-                DamageSource.causeMobDamage(ObjectInterop.entityOrNull(causingEntity) as? EntityLivingBase)
-            )
-
-            VanillaDamageType.MobProjectile -> ObjectInterop.damageSource(
-                EntityDamageSourceIndirect(
-                    "mob",
-                    ObjectInterop.entityOrNull(directEntity),
-                    ObjectInterop.entityOrNull(causingEntity)
-                ).setProjectile()
-            )
-
-            VanillaDamageType.PlayerAttack -> ObjectInterop.damageSource(
-                DamageSource.causePlayerDamage(ObjectInterop.entityOrNull(causingEntity) as? EntityPlayer)
-            )
-
-            VanillaDamageType.Arrow -> ObjectInterop.damageSource(
-                DamageSource.causeArrowDamage(
-                    ObjectInterop.entityOrNull(directEntity) as? EntityArrow,
-                    ObjectInterop.entityOrNull(causingEntity)
-                )
-            )
-
-            VanillaDamageType.Fireball -> ObjectInterop.damageSource(
-                EntityDamageSourceIndirect(
-                    "fireball",
-                    ObjectInterop.entityOrNull(directEntity) as? EntityFireball,
-                    ObjectInterop.entityOrNull(causingEntity)
-                ).setFireDamage().setProjectile()
-            )
-
+            VanillaDamageType.InFire -> DamageSource.IN_FIRE.damageSource()
+            VanillaDamageType.LightningBolt -> DamageSource.LIGHTNING_BOLT.damageSource()
+            VanillaDamageType.OnFire -> DamageSource.ON_FIRE.damageSource()
+            VanillaDamageType.Lava -> DamageSource.LAVA.damageSource()
+            VanillaDamageType.HotFloor -> DamageSource.HOT_FLOOR.damageSource()
+            VanillaDamageType.InWall -> DamageSource.IN_WALL.damageSource()
+            VanillaDamageType.Cramming -> DamageSource.CRAMMING.damageSource()
+            VanillaDamageType.Drown -> DamageSource.DROWN.damageSource()
+            VanillaDamageType.Starve -> DamageSource.STARVE.damageSource()
+            VanillaDamageType.Cactus -> DamageSource.CACTUS.damageSource()
+            VanillaDamageType.Fall -> DamageSource.FALL.damageSource()
+            VanillaDamageType.FlyIntoWall -> DamageSource.FLY_INTO_WALL.damageSource()
+            VanillaDamageType.FellOutOfWorld -> DamageSource.OUT_OF_WORLD.damageSource()
+            VanillaDamageType.Generic -> DamageSource.GENERIC.damageSource()
+            VanillaDamageType.Magic -> DamageSource.MAGIC.damageSource()
+            VanillaDamageType.Wither -> DamageSource.WITHER.damageSource()
+            VanillaDamageType.FallingAnvil -> DamageSource.ANVIL.damageSource()
+            VanillaDamageType.FallingBlock -> DamageSource.FALLING_BLOCK.damageSource()
+            VanillaDamageType.DragonBreath -> DamageSource.DRAGON_BREATH.damageSource()
+            VanillaDamageType.Fireworks -> DamageSource.FIREWORKS.damageSource()
+            VanillaDamageType.MobAttack -> DamageSource.causeMobDamage(causingEntity.entityOrNull() as? EntityLivingBase).damageSource()
+            VanillaDamageType.MobProjectile -> EntityDamageSourceIndirect("mob", directEntity.entityOrNull(), causingEntity.entityOrNull()).setProjectile().damageSource()
+            VanillaDamageType.PlayerAttack -> DamageSource.causePlayerDamage(causingEntity.entityOrNull() as? EntityPlayer).damageSource()
+            VanillaDamageType.Arrow -> DamageSource.causeArrowDamage(directEntity.entityOrNull() as? EntityArrow, causingEntity.entityOrNull()).damageSource()
+            VanillaDamageType.Fireball -> EntityDamageSourceIndirect("fireball", directEntity.entityOrNull() as? EntityFireball, causingEntity.entityOrNull()).setFireDamage().setProjectile().damageSource()
             VanillaDamageType.UnattributedFireball -> virtual(spec, directEntity, causingEntity, position)
-
-            VanillaDamageType.Thrown -> ObjectInterop.damageSource(
-                DamageSource.causeThrownDamage(
-                    ObjectInterop.entityOrNull(directEntity),
-                    ObjectInterop.entityOrNull(causingEntity)
-                )
-            )
-
-            VanillaDamageType.IndirectMagic -> ObjectInterop.damageSource(
-                DamageSource.causeIndirectMagicDamage(ObjectInterop.entityOrNull(directEntity), ObjectInterop.entityOrNull(causingEntity))
-            )
-
-            VanillaDamageType.Thorns -> ObjectInterop.damageSource(DamageSource.causeThornsDamage(ObjectInterop.entityOrNull(causingEntity)))
-
-            VanillaDamageType.Explosion -> ObjectInterop.damageSource(
-                @Suppress("USELESS_CAST")
-                DamageSource.causeExplosionDamage(null as? EntityLivingBase)
-            )
-
-            VanillaDamageType.PlayerExplosion -> ObjectInterop.damageSource(
-                EntityDamageSource(
-                    "explosion.player",
-                    ObjectInterop.entityOrNull(causingEntity)
-                ).setDifficultyScaled().setExplosion()
-            )
-
+            VanillaDamageType.Thrown -> DamageSource.causeThrownDamage(directEntity.entityOrNull(), causingEntity.entityOrNull()).damageSource()
+            VanillaDamageType.IndirectMagic -> DamageSource.causeIndirectMagicDamage(directEntity.entityOrNull(), causingEntity.entityOrNull()).damageSource()
+            VanillaDamageType.Thorns -> DamageSource.causeThornsDamage(causingEntity.entityOrNull()).damageSource()
+            VanillaDamageType.Explosion -> explosion()
+            VanillaDamageType.PlayerExplosion -> EntityDamageSource("explosion.player", causingEntity.entityOrNull()).setDifficultyScaled().setExplosion().damageSource()
             else -> virtual(spec, directEntity, causingEntity, position)
         }
     }
@@ -113,5 +66,10 @@ class DamageProvider : DamageProvider {
 
     private fun virtual(spec: VanillaDamageSourceSpec, directEntity: EntityAccess?, causingEntity: EntityAccess?, position: VectorView?): DamageSourceView {
         return VirtualDamageProvider.source(spec, directEntity, causingEntity, position)
+    }
+
+    private fun explosion(): DamageSourceView {
+        val causingEntity: EntityLivingBase? = null
+        return DamageSource.causeExplosionDamage(causingEntity).damageSource()
     }
 }
