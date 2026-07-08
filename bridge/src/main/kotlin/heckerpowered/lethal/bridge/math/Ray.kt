0475f6137cbb42dd476c690fbd4eb46365d6cdf6
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-08 21:41:13 +0530

    Add RayIntersectionContext class for optimized ray-box intersection caching

--- bridge/src/main/kotlin/heckerpowered/lethal/bridge/math/Ray.kt
+++ bridge/src/main/kotlin/heckerpowered/lethal/bridge/math/Ray.kt
@@ -32,3 +32,55 @@ interface RayView {
         return sqrt(distanceSquaredTo(point))
     }
 }
+
+/**
+ * Caches ray values reused by repeated ray-box intersection tests.
+ */
+class RayIntersectionContext private constructor(val ray: RayView, directionLength: Double, length: Double?) {
+    val maximumTime: Double = length?.let { it / directionLength } ?: Double.POSITIVE_INFINITY
+
+    val originX: Double
+    val originY: Double
+    val originZ: Double
+
+    val directionX: Double
+    val directionY: Double
+    val directionZ: Double
+
+    val directionXIsNearlyZero: Boolean
+    val directionYIsNearlyZero: Boolean
+    val directionZIsNearlyZero: Boolean
+
+    val inverseDirectionX: Double
+    val inverseDirectionY: Double
+    val inverseDirectionZ: Double
+
+    init {
+        val origin = ray.origin
+        originX = origin.x
+        originY = origin.y
+        originZ = origin.z
+
+        val direction = ray.direction
+        directionX = direction.x
+        directionY = direction.y
+        directionZ = direction.z
+
+        directionXIsNearlyZero = directionX.isNearlyZero()
+        directionYIsNearlyZero = directionY.isNearlyZero()
+        directionZIsNearlyZero = directionZ.isNearlyZero()
+
+        inverseDirectionX = if (directionXIsNearlyZero) 0.0 else 1.0 / directionX
+        inverseDirectionY = if (directionYIsNearlyZero) 0.0 else 1.0 / directionY
+        inverseDirectionZ = if (directionZIsNearlyZero) 0.0 else 1.0 / directionZ
+    }
+
+    companion object {
+        fun create(ray: RayView, length: Double? = null): RayIntersectionContext? {
+            val directionLength = ray.direction.length
+            if (directionLength.isNearlyZero()) return null
+
+            return RayIntersectionContext(ray, directionLength, length)
+        }
+    }
+}
