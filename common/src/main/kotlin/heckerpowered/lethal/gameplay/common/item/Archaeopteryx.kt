06ad5bf6ef7e19ef593f06696bb4c26557f88f87
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-11 10:56:47 -0400

    Add new firearms: Fortune and Archaeopteryx with localization and sound effects

--- common/src/main/kotlin/heckerpowered/lethal/gameplay/common/item/Archaeopteryx.kt
+++ common/src/main/kotlin/heckerpowered/lethal/gameplay/common/item/Archaeopteryx.kt
@@ -6,21 +6,29 @@
 package heckerpowered.lethal.gameplay.common.item
 
 import heckerpowered.bridge.adapter.entity.PlayerAccess
+import heckerpowered.bridge.adapter.entity.damagesource.DamageSources
+import heckerpowered.bridge.adapter.entity.damagesource.VanillaDamageType
 import heckerpowered.bridge.adapter.item.stack.ItemStackAccess
+import heckerpowered.bridge.adapter.world.raycast.raycastEntityHits
+import heckerpowered.bridge.math.Geometry
 import heckerpowered.bridge.resources.Identifier
 import heckerpowered.bridge.time.Frequency
 import heckerpowered.lethal.Constants
 import heckerpowered.lethal.gameplay.common.item.firearm.Firearm
 
-object TestFirearmItem : Firearm() {
+object Archaeopteryx : Firearm() {
     override val identifier: Identifier
-        get() = Constants.identifier("test_firearm")
+        get() = Constants.identifier("archaeopteryx")
 
     override fun getFrequency(player: PlayerAccess, weaponStack: ItemStackAccess): Frequency {
-        return Frequency.perSecond(2)
+        return Frequency.perMinute(650)
     }
 
     override fun shoot(player: PlayerAccess, weaponStack: ItemStackAccess) {
-
+        val damageSource = DamageSources.vanilla(VanillaDamageType.Generic, player, player)
+        player.world.raycastEntityHits(Geometry.ray(player.eyePosition, player.viewVector), 120.0, player)
+            .forEach {
+                it.entity.hurt(damageSource, 7.0)
+            }
     }
 }
