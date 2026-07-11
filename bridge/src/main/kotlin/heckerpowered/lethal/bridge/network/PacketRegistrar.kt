e97c56e2f2eb59a2db1fc434aa57050b67254f03
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-11 02:20:32 -0400

    Add PacketRegistrar interface for clientbound and serverbound payload registration

---
+++ bridge/src/main/kotlin/heckerpowered/lethal/bridge/network/PacketRegistrar.kt
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 heckerpowered
+ */
+
+package heckerpowered.lethal.bridge.network
+
+import heckerpowered.lethal.bridge.network.codec.StreamCodec
+
+interface PacketRegistrar {
+    fun <T : ClientboundPayload<T>> registerClientbound(type: Payload.Type<T>, codec: StreamCodec<StreamBuffer, T>): PayloadDefinition<T>
+
+    fun <T : ServerboundPayload<T>> registerServerbound(type: Payload.Type<T>, codec: StreamCodec<StreamBuffer, T>): PayloadDefinition<T>
+}
