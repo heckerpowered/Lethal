38d1105f593174f33d134b271d2844cc07a9953d
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-11 05:36:54 -0400

    Add ClientNetworking object for managing clientbound payloads

---
+++ bridge/src/main/kotlin/heckerpowered/bridge/network/ClientNetworking.kt
@@ -0,0 +1,26 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 heckerpowered
+ */
+
+package heckerpowered.bridge.network
+
+import heckerpowered.bridge.adapter.entity.PlayerAccess
+
+object ClientNetworking {
+    private val receivers = PayloadReceiverRegistry<Context>()
+
+    fun <T : ClientboundPayload<T>> registerReceiver(type: Payload.Type<T>, handler: (T, Context) -> Unit) {
+        receivers.register(type, handler)
+    }
+
+    fun handle(payload: Payload<*>, context: Context) {
+        receivers.handle(payload, context)
+    }
+
+    interface Context {
+        // TODO: ClientAccess
+
+        val player: PlayerAccess
+    }
+}
