6feb21ec2f6999a5e101e44da2965cbe10ef5241
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-11 10:17:30 -0400

    Initialize ModServerPlayNetworking in Entrypoint

--- common/src/main/kotlin/heckerpowered/lethal/gameplay/common/Entrypoint.kt
+++ common/src/main/kotlin/heckerpowered/lethal/gameplay/common/Entrypoint.kt
@@ -8,9 +8,11 @@ package heckerpowered.lethal.gameplay.common
 import heckerpowered.bridge.platform.Services
 import heckerpowered.bridge.platform.services.Entrypoint
 import heckerpowered.lethal.gameplay.common.item.TestItem
+import heckerpowered.lethal.gameplay.common.network.ModServerPlayNetworking
 
 class Entrypoint : Entrypoint {
     override fun onEntrypoint() {
+        ModServerPlayNetworking.onInitialize()
         Services.ItemRegistrar.register(TestItem())
     }
 }
