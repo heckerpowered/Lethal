36e7df4aee5cb952c7af0415b3d93dc14182b100
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-11 05:57:46 -0400

    Update message registration for Forge packet envelopes in ForgePayloadNetwork

--- forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/network/ForgePayloadNetwork.kt
+++ forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/network/ForgePayloadNetwork.kt
@@ -13,7 +13,9 @@ object ForgePayloadNetwork {
     private val Channel = NetworkRegistry.INSTANCE.newSimpleChannel(Constants.MOD_ID)
 
     init {
-        Channel.registerMessage(ClientboundForgePacketEnvelopeHandler(), ClientboundForgePacketEnvelope::class.java, 0, Side.CLIENT)
-        Channel.registerMessage(ServerboundForgePacketEnvelopeHandler(), ServerboundForgePacketEnvelope::class.java, 1, Side.SERVER)
+        Channel.registerMessage(ClientboundForgePacketEnvelopeHandler(), ClientboundForgePayloadEnvelope::class.java, 0, Side.CLIENT)
+        Channel.registerMessage(ServerboundForgePacketEnvelopeHandler(), ServerboundForgePayloadEnvelope::class.java, 1, Side.SERVER)
     }
+
+    fun onInitialize() {}
 }
