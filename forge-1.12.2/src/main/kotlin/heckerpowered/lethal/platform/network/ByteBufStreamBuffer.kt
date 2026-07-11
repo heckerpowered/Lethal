fefdb5d94fbefdb11de7005abdbbb17bde35685a
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-11 02:45:49 -0400

    Refactor ByteBufStreamBuffer methods for improved readability and consistency

--- forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/network/ByteBufStreamBuffer.kt
+++ forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/network/ByteBufStreamBuffer.kt
@@ -12,38 +12,39 @@ class ByteBufStreamBuffer(private val buffer: ByteBuf) : StreamBuffer {
     override val readableByteCount: Int
         get() = buffer.readableBytes()
 
-    override fun writeBoolean(value: Boolean) { buffer.writeBoolean(value) }
+    override fun writeBoolean(value: Boolean) {
+        buffer.writeBoolean(value)
+    }
     override fun writeByte(value: Byte) {
         buffer.writeByte(value.toInt())
     }
-
     override fun writeShort(value: Short) {
         buffer.writeShort(value.toInt())
     }
-
     override fun writeInt(value: Int) {
         buffer.writeInt(value)
     }
-
     override fun writeLong(value: Long) {
         buffer.writeLong(value)
     }
-
     override fun writeFloat(value: Float) {
         buffer.writeFloat(value)
     }
+    override fun writeDouble(value: Double) {
+        buffer.writeDouble(value)
+    }
 
-    override fun writeDouble(value: Double) {buffer.writeDouble(value)}
+    override fun writeBytes(value: ByteArray) {
+        buffer.writeBytes(value)
+    }
 
     override fun readBoolean() = buffer.readBoolean()
     override fun readByte(): Byte = buffer.readByte()
-
     override fun readShort(): Short = buffer.readShort()
     override fun readInt(): Int = buffer.readInt()
     override fun readLong(): Long = buffer.readLong()
     override fun readFloat(): Float = buffer.readFloat()
     override fun readDouble(): Double =  buffer.readDouble()
-
 
     override fun readBytes(byteCount: Int): ByteArray {
         require(byteCount >= 0) { "Byte count must not be negative: $byteCount" }
@@ -52,9 +53,5 @@ class ByteBufStreamBuffer(private val buffer: ByteBuf) : StreamBuffer {
         val value = ByteArray(byteCount)
         buffer.readBytes(value)
         return value
-    }
-
-    override fun writeBytes(value: ByteArray) {
-        buffer.writeBytes(value)
     }
 }
