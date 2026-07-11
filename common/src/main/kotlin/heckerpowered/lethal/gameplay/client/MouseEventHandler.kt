daf151ff0d69fc993b6dce0846a1dd2e82a56c60
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-11 10:57:01 -0400

    Handle mouse button input for firearm actions and send FireStatePayload to server

--- common/src/main/kotlin/heckerpowered/lethal/gameplay/client/MouseEventHandler.kt
+++ common/src/main/kotlin/heckerpowered/lethal/gameplay/client/MouseEventHandler.kt
@@ -5,11 +5,16 @@
 
 package heckerpowered.lethal.gameplay.client
 
+import heckerpowered.bridge.adapter.item.EquipmentSlot
+import heckerpowered.bridge.input.InputAction
 import heckerpowered.bridge.input.MouseButton
 import heckerpowered.bridge.input.MouseButtonEvent
 import heckerpowered.bridge.input.MouseButtonInputRule
+import heckerpowered.bridge.platform.Services
 import heckerpowered.bridge.rule.RuleRegistry
 import heckerpowered.bridge.rule.register
+import heckerpowered.lethal.gameplay.common.item.firearm.Gun
+import heckerpowered.lethal.gameplay.common.network.FireStatePayload
 
 object MouseEventHandler : MouseButtonInputRule {
     init {
@@ -21,7 +26,15 @@ object MouseEventHandler : MouseButtonInputRule {
 
     override fun onMouseButtonInput(event: MouseButtonEvent) {
         if (event.button != MouseButton.Left) return
+        val minecraft = Services.Platform.minecraft
+        val player = minecraft.player ?: return
+        if (player.getEquippedStack(EquipmentSlot.MainHand).item.form !is Gun &&
+            player.getEquippedStack(EquipmentSlot.OffHand).item.form !is Gun
+        ) {
+            return
+        }
+        event.cancel()
 
-        
+        Services.PayloadTransport.sendToServer(FireStatePayload(event.action == InputAction.Press))
     }
 }
