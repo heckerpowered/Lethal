82a7a35daa0bd3e45e72dd311880216559e87891
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-12 03:03:00 -0400

    Implement playSound method in HostedWorldAccess for sound playback

--- forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/adapter/world/HostedWorldAccess.kt
+++ forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/adapter/world/HostedWorldAccess.kt
@@ -6,10 +6,12 @@
 package heckerpowered.lethal.platform.adapter.world
 
 import heckerpowered.bridge.adapter.entity.EntityAccess
+import heckerpowered.bridge.adapter.sound.SoundPlayback
 import heckerpowered.bridge.adapter.world.WorldAccess
 import heckerpowered.bridge.adapter.world.raycast.EntityRayBucket
 import heckerpowered.bridge.math.BoxView
 import heckerpowered.bridge.math.RayView
+import heckerpowered.bridge.math.VectorView
 import heckerpowered.lethal.mixin.impl.WorldAccessImpl
 import net.minecraft.world.World
 
@@ -25,6 +27,10 @@ class HostedWorldAccess(val world: World) : WorldAccess {
 
     override fun getEntities(searchBox: BoxView): Sequence<EntityAccess> {
         return WorldAccessImpl.getEntities(world, searchBox)
+    }
+
+    override fun playSound(position: VectorView, playback: SoundPlayback) {
+        WorldAccessImpl.playSound(world, position, playback)
     }
 
     override fun getEntityRayBuckets(ray: RayView, length: Double): Sequence<EntityRayBucket> {
