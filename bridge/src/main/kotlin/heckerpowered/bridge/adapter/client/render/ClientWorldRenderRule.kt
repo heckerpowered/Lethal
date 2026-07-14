922987e5ee6ae152dbf1489a4afb7e6eb56e6a97
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-14 01:50:59 -0400

    Add ClientWorldRenderRule interface for world rendering events

---
+++ bridge/src/main/kotlin/heckerpowered/bridge/adapter/client/render/ClientWorldRenderRule.kt
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 heckerpowered
+ */
+
+package heckerpowered.bridge.adapter.client.render
+
+interface ClientWorldRenderRule {
+    fun onWorldRender(context: ClientWorldRenderContext)
+}
