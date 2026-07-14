77314e5074b34e07edebeb5c22ed73a0b4fe05fa
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-14 01:50:02 -0400

    Initialize DivineShotEffect in the client entry point

--- common/src/main/kotlin/heckerpowered/lethal/gameplay/client/Entrypoint.kt
+++ common/src/main/kotlin/heckerpowered/lethal/gameplay/client/Entrypoint.kt
@@ -7,11 +7,13 @@ package heckerpowered.lethal.gameplay.client
 
 import heckerpowered.bridge.platform.services.ClientEntrypoint
 import heckerpowered.lethal.gameplay.client.network.ModClientPlayNetworking
+import heckerpowered.lethal.gameplay.client.render.DivineShotEffect
 
 class Entrypoint : ClientEntrypoint {
     override fun onEntrypoint() {
         MouseEventHandler.onInitialize()
         BloomTest.onInitialize()
+        DivineShotEffect.onInitialize()
         ModClientPlayNetworking.onInitialize()
     }
 }
