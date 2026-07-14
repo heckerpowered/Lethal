303ffefbeea592bdea6603af91a73e3303772399
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-14 01:50:53 -0400

    Add ClientWorldRenderPipeline for handling world rendering events

---
+++ forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/render/ClientWorldRenderPipeline.kt
@@ -0,0 +1,25 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 heckerpowered
+ */
+
+package heckerpowered.lethal.platform.render
+
+import heckerpowered.bridge.adapter.client.render.ClientWorldRenderRule
+import heckerpowered.bridge.rule.RuleRegistry
+import heckerpowered.bridge.rule.forEach
+import heckerpowered.lethal.Constants
+import net.minecraftforge.client.event.RenderWorldLastEvent
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.relauncher.Side
+
+@Mod.EventBusSubscriber(modid = Constants.MOD_ID, value = [Side.CLIENT])
+object ClientWorldRenderPipeline {
+    @SubscribeEvent
+    @JvmStatic
+    fun onRenderWorldLast(event: RenderWorldLastEvent) {
+        val context = ForgeClientWorldRenderContext(event.partialTicks)
+        RuleRegistry.forEach<ClientWorldRenderRule> { rule -> rule.onWorldRender(context) }
+    }
+}
