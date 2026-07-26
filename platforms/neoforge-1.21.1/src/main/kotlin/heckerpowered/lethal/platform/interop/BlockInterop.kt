d7b730517238b303f233550c41ade17cc9fa3b5a
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-26 08:45:07 -0400

    refactor: rename BlockStateAccess and BlockState extension functions for clarity

--- platforms/neoforge-1.21.1/src/main/kotlin/heckerpowered/lethal/platform/interop/BlockInterop.kt
+++ platforms/neoforge-1.21.1/src/main/kotlin/heckerpowered/lethal/platform/interop/BlockInterop.kt
@@ -8,6 +8,6 @@ package heckerpowered.lethal.platform.interop
 import heckerpowered.bridge.adapter.block.BlockStateAccess
 import net.minecraft.world.level.block.state.BlockState
 
-fun BlockStateAccess.blockState() = requireHost<BlockState>(this)
+fun BlockStateAccess.asHost() = requireHost<BlockState>(this)
 
-fun BlockState.blockState() = requireAccess<BlockStateAccess>(this)
+fun BlockState.asView() = requireAccess<BlockStateAccess>(this)
