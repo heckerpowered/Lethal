7fc0c5a8160b46a0fdea09ec3fb98011f0f54abf
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-26 08:15:38 -0400

    refactor: rename ForgePlatform.kt for improved clarity

--- platforms/forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/services/ForgePlatform.kt
+++ platforms/forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/services/ForgePlatform.kt
@@ -5,9 +5,7 @@
 
 package heckerpowered.lethal.platform.services
 
-import heckerpowered.bridge.adapter.client.MinecraftAccess
 import heckerpowered.bridge.platform.services.ModPlatform
-import net.minecraft.client.Minecraft
 import net.minecraftforge.fml.common.Loader
 import net.minecraftforge.fml.relauncher.FMLLaunchHandler
 
@@ -33,7 +31,4 @@ class ForgePlatform : ModPlatform {
      */
     override val isDevelopmentEnvironment: Boolean
         get() = FMLLaunchHandler.isDeobfuscatedEnvironment()
-
-    override val minecraft: MinecraftAccess
-        get() = Minecraft.getMinecraft() as MinecraftAccess
 }
