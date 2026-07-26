a4db5c96c01538340551374bd1887b2d0c97998b
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-26 08:48:13 -0400

    Remove unused classes

--- common/src/main/kotlin/heckerpowered/lethal/gameplay/common/item/ZeusBlack.kt
+++
@@ -1,8 +0,0 @@
-/*
- * SPDX-License-Identifier: MIT
- * Copyright (c) 2026 heckerpowered
- */
-
-package heckerpowered.lethal.gameplay.common.item
-
-object ZeusBlack : Zeus("zeus_black", 10_000_000.0)
