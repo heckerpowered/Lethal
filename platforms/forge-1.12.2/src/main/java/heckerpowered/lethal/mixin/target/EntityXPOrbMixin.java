f9d46d16cc36a367fee0e770288037cd57ef769a
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-28 12:54:20 -0400

    refactor: simplify EntityXPOrbMixin by removing unnecessary method and using @Shadow for getXpValue

--- platforms/forge-1.12.2/src/main/java/heckerpowered/lethal/mixin/target/EntityXPOrbMixin.java
+++ platforms/forge-1.12.2/src/main/java/heckerpowered/lethal/mixin/target/EntityXPOrbMixin.java
@@ -10,21 +10,15 @@ import net.minecraft.entity.item.EntityXPOrb;
 import org.spongepowered.asm.mixin.Implements;
 import org.spongepowered.asm.mixin.Interface;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(EntityXPOrb.class)
 @Implements(@Interface(iface = ExperienceOrbAccess.class, prefix = "experienceOrbAccess$"))
 abstract class EntityXPOrbMixin {
-    @Unique
-    private EntityXPOrb lethal$self() {
-        return (EntityXPOrb) (Object) this;
-    }
+    @Shadow
+    public abstract int getXpValue();
 
     public int experienceOrbAccess$getExperiencePoints() {
-        return lethal$self().getXpValue();
-    }
-
-    public void experienceOrbAccess$consume() {
-        lethal$self().setDead();
+        return getXpValue();
     }
 }
