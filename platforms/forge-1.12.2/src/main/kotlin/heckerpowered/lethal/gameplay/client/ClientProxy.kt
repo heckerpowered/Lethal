2753fe6f876b9fc04a5b38317d2ff209344ef27e
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-28 12:13:47 -0400

    feat: enhance ClientProxy with initialization for entity renderers and key bindings

--- platforms/forge-1.12.2/src/main/kotlin/heckerpowered/lethal/gameplay/client/ClientProxy.kt
+++ platforms/forge-1.12.2/src/main/kotlin/heckerpowered/lethal/gameplay/client/ClientProxy.kt
@@ -7,12 +7,21 @@ package heckerpowered.lethal.gameplay.client
 
 import heckerpowered.bridge.platform.Services
 import heckerpowered.bridge.platform.services.ClientEntrypoint
+import heckerpowered.lethal.gameplay.client.render.ForgeEntityRenderers
 import heckerpowered.lethal.gameplay.common.CommonProxy
+import heckerpowered.lethal.platform.input.ForgeKeyBindings
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 
 class ClientProxy : CommonProxy() {
     override fun preInitialize(event: FMLPreInitializationEvent) {
         super.preInitialize(event)
+        ForgeEntityRenderers.onInitialize()
         Services.callEntrypoints<ClientEntrypoint>()
+    }
+
+    override fun initialize(event: FMLInitializationEvent) {
+        super.initialize(event)
+        ForgeKeyBindings.registerAll()
     }
 }
