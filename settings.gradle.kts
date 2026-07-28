5ac6038eb540196fcdbd67319e5ff4f8966760f7
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-28 07:28:57 -0400

    feat: update project structure and include additional platforms in settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,39 +7,18 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
-        maven("https://repo.spongepowered.org/maven")
-    }
-
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.toString() == "org.spongepowered.mixin") {
-                useModule("org.spongepowered:mixingradle:${requested.version}")
-            }
-        }
-    }
-}
-dependencyResolutionManagement {
-    // Use Maven Central as the default repository (where Gradle will download dependencies) in all subprojects.
-    @Suppress("UnstableApiUsage")
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-        maven("https://repo.spongepowered.org/maven")
     }
 }
 
 plugins {
     // Use the Foojay Toolchains plugin to automatically download JDKs required by subprojects.
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.+"
 }
-
-// Include the `app` and `utils` subprojects in the build.
-// If there are changes in only one of the projects, Gradle will rebuild only the one that has changed.
-// Learn more about structuring projects with Gradle - https://docs.gradle.org/8.7/userguide/multi_project_builds.html
-// include(":app")
-// include(":utils")
 
 rootProject.name = "Lethal"
 include("common")
 include("bridge")
-include("forge-1.12.2")
+include("platforms:forge-1.12.2")
+include("platforms:neoforge-1.21.1")
+include("render-hardware-interface")
+include("render-hardware-interface-codegen")
