c0125c2d71e455898056b244c6adda05afa99ecc
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-29 09:42:41 -0400

    feat: register SkillActivationPayload and ZeusChainPayload for networking

--- common/src/main/kotlin/heckerpowered/lethal/gameplay/common/network/ModServerPlayNetworking.kt
+++ common/src/main/kotlin/heckerpowered/lethal/gameplay/common/network/ModServerPlayNetworking.kt
@@ -9,11 +9,17 @@ import heckerpowered.bridge.network.PayloadTypeRegistry
 import heckerpowered.bridge.network.ServerPlayNetworking
 
 object ModServerPlayNetworking {
-    fun onInitialize() {
+    init {
         PayloadTypeRegistry.ServerboundPlay.register(FireStatePayload.Type, FireStatePayload.Codec)
+        PayloadTypeRegistry.ServerboundPlay.register(SkillActivationPayload.Type, SkillActivationPayload.Codec)
 
+        PayloadTypeRegistry.ClientboundPlay.register(ZeusChainPayload.Type, ZeusChainPayload.Codec)
         PayloadTypeRegistry.ClientboundPlay.register(ZeusShotPayload.Type, ZeusShotPayload.Codec)
 
         ServerPlayNetworking.registerReceiver(FireStatePayload.Type, FireStatePayload::handle)
+        ServerPlayNetworking.registerReceiver(SkillActivationPayload.Type, SkillActivationPayload::handle)
+    }
+
+    fun onInitialize() {
     }
 }
