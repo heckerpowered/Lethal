ad135e6fa6dee0bf67f066eed4891fe3db16798f
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-29 09:43:32 -0400

    feat: update Lethal creative mode tab to include EnhancedFortune and Chaos items

--- common/src/main/kotlin/heckerpowered/lethal/gameplay/common/creativetab/ModCreativeModeTabs.kt
+++ common/src/main/kotlin/heckerpowered/lethal/gameplay/common/creativetab/ModCreativeModeTabs.kt
@@ -9,22 +9,37 @@ import heckerpowered.bridge.adapter.item.creativetab.CreativeModeTabBlueprint
 import heckerpowered.bridge.adapter.item.creativetab.CreativeModeTabRegistry
 import heckerpowered.lethal.Constants
 import heckerpowered.lethal.gameplay.common.item.Archaeopteryx
+import heckerpowered.lethal.gameplay.common.item.Chaos
+import heckerpowered.lethal.gameplay.common.item.EnhancedFortune
 import heckerpowered.lethal.gameplay.common.item.Fortune
 import heckerpowered.lethal.gameplay.common.item.Zeus
-import heckerpowered.lethal.gameplay.common.item.ZeusBlack
+import heckerpowered.lethal.gameplay.common.item.ZeusBlackGold
 import heckerpowered.lethal.gameplay.common.item.ZeusGlowSquid
 import heckerpowered.lethal.gameplay.common.item.ZeusGolden
 import heckerpowered.lethal.gameplay.common.item.ZeusSculk
 
 object ModCreativeModeTabs {
-    val Lethal = CreativeModeTabBlueprint(
-        identifier = Constants.identifier("main"),
-        titleTranslationKey = "itemGroup.lethal",
-        icon = Fortune,
-        items = listOf(Archaeopteryx, Fortune, Zeus, ZeusGolden, ZeusBlack, ZeusGlowSquid, ZeusSculk),
+    val Lethal = register(
+        CreativeModeTabBlueprint(
+            Constants.identifier("main"), "itemGroup.lethal", Fortune,
+            listOf(
+                Archaeopteryx,
+                Fortune,
+                EnhancedFortune,
+                Chaos,
+                Zeus,
+                ZeusGolden,
+                ZeusBlackGold,
+                ZeusGlowSquid,
+                ZeusSculk,
+            ),
+        ),
     )
 
-    fun register() {
-        CreativeModeTabRegistry.register(Lethal)
+    fun onInitialize() {
+    }
+
+    private fun register(blueprint: CreativeModeTabBlueprint): CreativeModeTabBlueprint {
+        return CreativeModeTabRegistry.register(blueprint)
     }
 }
