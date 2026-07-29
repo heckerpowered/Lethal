f0e57a8db5af4d490a5669c8f2424897ea0a3a5b
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-29 09:59:30 -0400

    refactor: rename functions in GeometryInterop for clarity and consistency

--- platforms/forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/interop/GeometryInterop.kt
+++ platforms/forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/interop/GeometryInterop.kt
@@ -6,62 +6,83 @@
 package heckerpowered.lethal.platform.interop
 
 import heckerpowered.bridge.math.*
+import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.AxisAlignedBB
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Vec3d
 
 object GeometryInterop {
     @JvmStatic
-    fun vector(value: VectorView): Vec3d {
+    fun asHost(value: VectorView): Vec3d {
         return value as? Vec3d ?: Vec3d(value.x, value.y, value.z)
     }
 
     @JvmStatic
-    fun vector(value: Vec3d): VectorView {
+    fun asView(value: Vec3d): VectorView {
         return value as? VectorView ?: Geometry.vector(value.x, value.y, value.z)
     }
 
     @JvmStatic
-    fun box(value: BoxView): AxisAlignedBB {
+    fun asHost(value: BoxView): AxisAlignedBB {
         return value as? AxisAlignedBB ?: AxisAlignedBB(value.minX, value.minY, value.minZ, value.maxX, value.maxY, value.maxZ)
     }
 
     @JvmStatic
-    fun box(value: AxisAlignedBB): BoxView {
+    fun asView(value: AxisAlignedBB): BoxView {
         return value as? BoxView ?: Geometry.box(value.minX, value.minY, value.minZ, value.maxX, value.maxY, value.maxZ)
     }
 
-    @JvmStatic
-    fun blockPosition(value: BlockPositionView): BlockPos {
+    fun asHost(value: BlockPositionView): BlockPos {
         return value as? BlockPos ?: BlockPos(value.x, value.y, value.z)
     }
 
-    @JvmStatic
-    fun blockPosition(value: BlockPos): BlockPositionView {
+    fun asView(value: BlockPos): BlockPositionView {
         return value as? BlockPositionView ?: BlockPositions.of(value.x, value.y, value.z)
     }
 }
 
-fun VectorView.vector(): Vec3d {
-    return GeometryInterop.vector(this)
+fun VectorView.asHost(): Vec3d {
+    return GeometryInterop.asHost(this)
 }
 
-fun Vec3d.vector(): VectorView {
-    return GeometryInterop.vector(this)
+fun Vec3d.asView(): VectorView {
+    return GeometryInterop.asView(this)
 }
 
-fun BoxView.box(): AxisAlignedBB {
-    return GeometryInterop.box(this)
+fun BoxView.asHost(): AxisAlignedBB {
+    return GeometryInterop.asHost(this)
 }
 
-fun AxisAlignedBB.box(): BoxView {
-    return GeometryInterop.box(this)
+fun AxisAlignedBB.asView(): BoxView {
+    return GeometryInterop.asView(this)
 }
 
-fun BlockPositionView.blockPosition(): BlockPos {
-    return GeometryInterop.blockPosition(this)
+fun BlockPositionView.asHost(): BlockPos {
+    return GeometryInterop.asHost(this)
 }
 
-fun BlockPos.blockPosition(): BlockPositionView {
-    return GeometryInterop.blockPosition(this)
+fun BlockPos.asView(): BlockPositionView {
+    return GeometryInterop.asView(this)
+}
+
+fun EnumFacing.asView(): BlockDirection {
+    return when (this) {
+        EnumFacing.DOWN -> BlockDirection.Down
+        EnumFacing.UP -> BlockDirection.Up
+        EnumFacing.NORTH -> BlockDirection.North
+        EnumFacing.SOUTH -> BlockDirection.South
+        EnumFacing.WEST -> BlockDirection.West
+        EnumFacing.EAST -> BlockDirection.East
+    }
+}
+
+fun BlockDirection.asHost(): EnumFacing {
+    return when (this) {
+        BlockDirection.Down -> EnumFacing.DOWN
+        BlockDirection.Up -> EnumFacing.UP
+        BlockDirection.North -> EnumFacing.NORTH
+        BlockDirection.South -> EnumFacing.SOUTH
+        BlockDirection.West -> EnumFacing.WEST
+        BlockDirection.East -> EnumFacing.EAST
+    }
 }
