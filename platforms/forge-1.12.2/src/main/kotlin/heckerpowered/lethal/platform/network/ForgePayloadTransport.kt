b2e19194d370d2572586f91a28f0656d7c25edc8
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-07-29 10:24:36 -0400

    feat: update ForgePayloadTransport to use asHost for player entity conversion

--- platforms/forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/network/ForgePayloadTransport.kt
+++ platforms/forge-1.12.2/src/main/kotlin/heckerpowered/lethal/platform/network/ForgePayloadTransport.kt
@@ -5,12 +5,13 @@
 
 package heckerpowered.lethal.platform.network
 
+import heckerpowered.bridge.adapter.entity.EntityAccess
 import heckerpowered.bridge.adapter.entity.ServerPlayerAccess
 import heckerpowered.bridge.network.ClientboundPayload
 import heckerpowered.bridge.network.PayloadTransport
 import heckerpowered.bridge.network.ServerboundPayload
 import heckerpowered.lethal.Constants
-import heckerpowered.lethal.platform.interop.ObjectInterop
+import heckerpowered.lethal.platform.interop.asHost
 import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraftforge.fml.common.network.NetworkRegistry
 import net.minecraftforge.fml.relauncher.Side
@@ -28,7 +29,15 @@ class ForgePayloadTransport : PayloadTransport {
     }
 
     override fun sendToPlayer(player: ServerPlayerAccess, payload: ClientboundPayload<*>) {
-        val player = ObjectInterop.entity<EntityPlayerMP>(player)
+        val player = player.asHost() as EntityPlayerMP
         channel.sendTo(ClientboundForgePayloadEnvelope(payload), player)
+    }
+
+    override fun sendToPlayersTracking(entity: EntityAccess, payload: ClientboundPayload<*>) {
+        val nativeEntity = entity.asHost()
+        channel.sendToAllTracking(ClientboundForgePayloadEnvelope(payload), nativeEntity)
+        if (nativeEntity is EntityPlayerMP) {
+            channel.sendTo(ClientboundForgePayloadEnvelope(payload), nativeEntity)
+        }
     }
 }
