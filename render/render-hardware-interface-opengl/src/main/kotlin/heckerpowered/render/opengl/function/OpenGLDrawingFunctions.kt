dbba63a34223ec533826380e54cbb209fb585468
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-08-10 04:46:13 -0400

    feat: add context extension for drawArrays function in OpenGLDrawingFunctions

--- render/render-hardware-interface-opengl/src/main/kotlin/heckerpowered/render/opengl/function/OpenGLDrawingFunctions.kt
+++ render/render-hardware-interface-opengl/src/main/kotlin/heckerpowered/render/opengl/function/OpenGLDrawingFunctions.kt
@@ -13,3 +13,7 @@ import heckerpowered.render.opengl.PrimitiveMode
 interface OpenGLDrawingFunctions {
     fun drawArrays(mode: PrimitiveMode, firstVertex: Int, vertexCount: Int)
 }
+
+context(function: OpenGLDrawingFunctions)
+fun drawArrays(mode: PrimitiveMode, firstVertex: Int, vertexCount: Int) =
+    function.drawArrays(mode, firstVertex, vertexCount)
