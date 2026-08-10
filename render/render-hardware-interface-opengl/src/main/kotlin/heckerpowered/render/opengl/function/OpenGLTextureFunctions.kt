4b5f4a33a97bc9858105f05b9de2ba65389ab074
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-08-10 04:47:00 -0400

    feat: add context functions for texture management in OpenGLTextureFunctions

--- render/render-hardware-interface-opengl/src/main/kotlin/heckerpowered/render/opengl/function/OpenGLTextureFunctions.kt
+++ render/render-hardware-interface-opengl/src/main/kotlin/heckerpowered/render/opengl/function/OpenGLTextureFunctions.kt
@@ -29,3 +29,38 @@ interface OpenGLTextureFunctions {
     fun getTextureParameter(parameter: Int): Int
     fun deleteTexture(texture: TextureName)
 }
+
+context(function: OpenGLTextureFunctions)
+fun createTexture(): TextureName = function.createTexture()
+
+context(function: OpenGLTextureFunctions)
+fun getActiveTextureUnit(): TextureUnit = function.getActiveTextureUnit()
+
+context(function: OpenGLTextureFunctions)
+fun activeTexture(unit: TextureUnit) = function.activeTexture(unit)
+
+context(function: OpenGLTextureFunctions)
+fun getBoundTexture2D(): TextureName = function.getBoundTexture2D()
+
+context(function: OpenGLTextureFunctions)
+fun bindTexture2D(texture: TextureName) = function.bindTexture2D(texture)
+
+context(function: OpenGLTextureFunctions)
+fun textureImage2D(
+    level: Int,
+    internalFormat: Int,
+    width: Int,
+    height: Int,
+    format: Int,
+    type: Int,
+    pixels: ByteBuffer?,
+) = function.textureImage2D(level, internalFormat, width, height, format, type, pixels)
+
+context(function: OpenGLTextureFunctions)
+fun textureParameter(parameter: Int, value: Int) = function.textureParameter(parameter, value)
+
+context(function: OpenGLTextureFunctions)
+fun getTextureParameter(parameter: Int): Int = function.getTextureParameter(parameter)
+
+context(function: OpenGLTextureFunctions)
+fun deleteTexture(texture: TextureName) = function.deleteTexture(texture)
