efeeb6057cda7b8129c97d6a96d7f805ecea0cfc
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-08-10 04:46:42 -0400

    feat: add context functions for shader operations in OpenGLShaderFunctions

--- render/render-hardware-interface-opengl/src/main/kotlin/heckerpowered/render/opengl/function/OpenGLShaderFunctions.kt
+++ render/render-hardware-interface-opengl/src/main/kotlin/heckerpowered/render/opengl/function/OpenGLShaderFunctions.kt
@@ -39,3 +39,71 @@ interface OpenGLShaderFunctions {
     fun uniformFloat4(location: UniformLocation, x: Float, y: Float, z: Float, w: Float)
     fun uniformMatrix4(location: UniformLocation, transpose: Boolean, value: FloatBuffer)
 }
+
+context(function: OpenGLShaderFunctions)
+fun createShader(type: ShaderType): ShaderName = function.createShader(type)
+
+context(function: OpenGLShaderFunctions)
+fun shaderSource(shader: ShaderName, source: CharSequence) = function.shaderSource(shader, source)
+
+context(function: OpenGLShaderFunctions)
+fun compileShader(shader: ShaderName) = function.compileShader(shader)
+
+context(function: OpenGLShaderFunctions)
+fun getShaderCompileStatus(shader: ShaderName): Boolean = function.getShaderCompileStatus(shader)
+
+context(function: OpenGLShaderFunctions)
+fun getShaderInfoLog(shader: ShaderName): String = function.getShaderInfoLog(shader)
+
+context(function: OpenGLShaderFunctions)
+fun deleteShader(shader: ShaderName) = function.deleteShader(shader)
+
+context(function: OpenGLShaderFunctions)
+fun createProgram(): ProgramName = function.createProgram()
+
+context(function: OpenGLShaderFunctions)
+fun attachShader(program: ProgramName, shader: ShaderName) = function.attachShader(program, shader)
+
+context(function: OpenGLShaderFunctions)
+fun bindVertexAttributeLocation(program: ProgramName, index: VertexAttributeIndex, name: CharSequence) =
+    function.bindVertexAttributeLocation(program, index, name)
+
+context(function: OpenGLShaderFunctions)
+fun linkProgram(program: ProgramName) = function.linkProgram(program)
+
+context(function: OpenGLShaderFunctions)
+fun getProgramLinkStatus(program: ProgramName): Boolean = function.getProgramLinkStatus(program)
+
+context(function: OpenGLShaderFunctions)
+fun getProgramInfoLog(program: ProgramName): String = function.getProgramInfoLog(program)
+
+context(function: OpenGLShaderFunctions)
+fun useProgram(program: ProgramName) = function.useProgram(program)
+
+context(function: OpenGLShaderFunctions)
+fun deleteProgram(program: ProgramName) = function.deleteProgram(program)
+
+context(function: OpenGLShaderFunctions)
+fun getUniformLocation(program: ProgramName, name: CharSequence): UniformLocation =
+    function.getUniformLocation(program, name)
+
+context(function: OpenGLShaderFunctions)
+fun uniformInt(location: UniformLocation, value: Int) = function.uniformInt(location, value)
+
+context(function: OpenGLShaderFunctions)
+fun uniformFloat(location: UniformLocation, value: Float) = function.uniformFloat(location, value)
+
+context(function: OpenGLShaderFunctions)
+fun uniformFloat2(location: UniformLocation, x: Float, y: Float) = function.uniformFloat2(location, x, y)
+
+context(function: OpenGLShaderFunctions)
+fun uniformFloat3(location: UniformLocation, x: Float, y: Float, z: Float) =
+    function.uniformFloat3(location, x, y, z)
+
+context(function: OpenGLShaderFunctions)
+fun uniformFloat4(location: UniformLocation, x: Float, y: Float, z: Float, w: Float) =
+    function.uniformFloat4(location, x, y, z, w)
+
+context(function: OpenGLShaderFunctions)
+fun uniformMatrix4(location: UniformLocation, transpose: Boolean, value: FloatBuffer) =
+    function.uniformMatrix4(location, transpose, value)
