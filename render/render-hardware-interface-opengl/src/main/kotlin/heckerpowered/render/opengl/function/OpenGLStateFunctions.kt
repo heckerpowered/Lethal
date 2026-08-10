7eb488c18a3cd70940790c7a8841f1042aee1e92
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-08-10 04:46:54 -0400

    feat: add context functions for OpenGL state management in OpenGLStateFunctions

--- render/render-hardware-interface-opengl/src/main/kotlin/heckerpowered/render/opengl/function/OpenGLStateFunctions.kt
+++ render/render-hardware-interface-opengl/src/main/kotlin/heckerpowered/render/opengl/function/OpenGLStateFunctions.kt
@@ -5,6 +5,7 @@
 
 package heckerpowered.render.opengl.function
 
+import heckerpowered.render.opengl.BlendEquation
 import java.nio.IntBuffer
 
 /**
@@ -25,7 +26,7 @@ interface OpenGLStateFunctions {
 
     fun blendFunctionSeparate(sourceRgbFactor: Int, destinationRgbFactor: Int, sourceAlphaFactor: Int, destinationAlphaFactor: Int)
 
-    fun blendEquation(equation: Int)
+    fun blendEquation(equation: BlendEquation)
     fun depthFunction(compareFunction: Int)
     fun depthMask(enabled: Boolean)
     fun cullFace(mode: Int)
@@ -35,3 +36,55 @@ interface OpenGLStateFunctions {
     fun clearDepth(depth: Double)
     fun clear(mask: Int)
 }
+
+context(function: OpenGLStateFunctions)
+fun getInteger(parameter: Int): Int = function.getInteger(parameter)
+
+context(function: OpenGLStateFunctions)
+fun getIntegers(parameter: Int, destination: IntBuffer) = function.getIntegers(parameter, destination)
+
+context(function: OpenGLStateFunctions)
+fun isEnabled(capability: Int): Boolean = function.isEnabled(capability)
+
+context(function: OpenGLStateFunctions)
+fun enable(capability: Int) = function.enable(capability)
+
+context(function: OpenGLStateFunctions)
+fun disable(capability: Int) = function.disable(capability)
+
+context(function: OpenGLStateFunctions)
+fun viewport(x: Int, y: Int, width: Int, height: Int) = function.viewport(x, y, width, height)
+
+context(function: OpenGLStateFunctions)
+fun blendFunctionSeparate(
+    sourceRgbFactor: Int,
+    destinationRgbFactor: Int,
+    sourceAlphaFactor: Int,
+    destinationAlphaFactor: Int,
+) = function.blendFunctionSeparate(sourceRgbFactor, destinationRgbFactor, sourceAlphaFactor, destinationAlphaFactor)
+
+context(function: OpenGLStateFunctions)
+fun blendEquation(equation: BlendEquation) = function.blendEquation(equation)
+
+context(function: OpenGLStateFunctions)
+fun depthFunction(compareFunction: Int) = function.depthFunction(compareFunction)
+
+context(function: OpenGLStateFunctions)
+fun depthMask(enabled: Boolean) = function.depthMask(enabled)
+
+context(function: OpenGLStateFunctions)
+fun cullFace(mode: Int) = function.cullFace(mode)
+
+context(function: OpenGLStateFunctions)
+fun colorMask(red: Boolean, green: Boolean, blue: Boolean, alpha: Boolean) =
+    function.colorMask(red, green, blue, alpha)
+
+context(function: OpenGLStateFunctions)
+fun clearColor(red: Float, green: Float, blue: Float, alpha: Float) =
+    function.clearColor(red, green, blue, alpha)
+
+context(function: OpenGLStateFunctions)
+fun clearDepth(depth: Double) = function.clearDepth(depth)
+
+context(function: OpenGLStateFunctions)
+fun clear(mask: Int) = function.clear(mask)
