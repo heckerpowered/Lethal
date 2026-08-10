70bff819a2bd7e0902e940c743ecf5acc338b2ad
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-08-10 04:46:33 -0400

    feat: add context functions for OpenGLSamplerFunctions interface

--- render/render-hardware-interface-opengl/src/main/kotlin/heckerpowered/render/opengl/function/OpenGLSamplerFunctions.kt
+++ render/render-hardware-interface-opengl/src/main/kotlin/heckerpowered/render/opengl/function/OpenGLSamplerFunctions.kt
@@ -15,8 +15,27 @@ import heckerpowered.render.opengl.TextureUnit
  */
 interface OpenGLSamplerFunctions {
     fun createSampler(): SamplerName
-    fun getBoundSampler(unit: TextureUnit): SamplerName
+
+    /** Returns the sampler bound to the currently active texture unit. */
+    fun getBoundSampler(): SamplerName
+
     fun bindSampler(unit: TextureUnit, sampler: SamplerName)
     fun samplerParameter(sampler: SamplerName, parameter: Int, value: Int)
     fun deleteSampler(sampler: SamplerName)
 }
+
+context(function: OpenGLSamplerFunctions)
+fun createSampler(): SamplerName = function.createSampler()
+
+context(function: OpenGLSamplerFunctions)
+fun getBoundSampler(): SamplerName = function.getBoundSampler()
+
+context(function: OpenGLSamplerFunctions)
+fun bindSampler(unit: TextureUnit, sampler: SamplerName) = function.bindSampler(unit, sampler)
+
+context(function: OpenGLSamplerFunctions)
+fun samplerParameter(sampler: SamplerName, parameter: Int, value: Int) =
+    function.samplerParameter(sampler, parameter, value)
+
+context(function: OpenGLSamplerFunctions)
+fun deleteSampler(sampler: SamplerName) = function.deleteSampler(sampler)
